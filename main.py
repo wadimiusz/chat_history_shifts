eea8bdbe37b64c9a9ed569218aab61cb9d56f343
Vadim Fomin <wadimiusz@gmail.com>
2022-11-14 23:39:48 +0400

    updates!

--- main.py
+++ main.py
@@ -58,8 +58,8 @@ class Message:
 class ShiftsDetector:
     def __init__(self):
         self._args: Namespace | None = None
-        self._slices: list[list[str]] | None = None
-        self._models: list[Word2Vec] | None = None
+        self._name_to_slice: dict[str, list[str]] | None = None
+        self._name_to_model: dict[str, Word2Vec] | None = None
         self.morph_analyzer = MorphAnalyzer()
 
     @property
@@ -74,9 +74,9 @@ class ShiftsDetector:
         return self._args
 
     @property
-    def slices(self) -> list[list[str]]:
-        if self._slices is None:
-            year_to_messages: dict[int, list[str]] = defaultdict(list)
+    def name_to_slice(self) -> dict[str, list[str]]:
+        if self._name_to_slice is None:
+            name_to_slice: dict[str, list[str]] = defaultdict(list)
             logger.info("Loading messages from the JSON files...")
             all_messages: list[list[dict[str, Any]]] = list()
 
@@ -84,53 +84,61 @@ class ShiftsDetector:
                 with open(path) as f:
                     history = json.load(f)
 
-                all_messages.append(history["messages"])
+                for chat in history["chats"]["list"]:
+                    all_messages.append(chat["messages"])
 
             for messages in all_messages:
                 for message_dict in messages:
                     if message_dict["type"] == "message":
                         message = Message.from_dict(message_dict)
                         if len(message.text) > 0 and (self.args.users is None or message.user in self.args.users):
-                            year_to_messages[message.year].append(message.text)
+                            name_to_slice[f"{message.year}"].append(message.text)
 
-            self._slices = [year_to_messages[year] for year in sorted(year_to_messages.keys())]
+            self._name_to_slice = {key: name_to_slice[key] for key in sorted(name_to_slice.keys(), key=lambda x: int(x))}
             logger.info("Messages loaded")
 
-        return self._slices
+        return self._name_to_slice
 
     def normalize(self, text: str) -> list[str]:
         tokens = [lemmatize(morph_analyzer=self.morph_analyzer, word=token).lower() for token in re.findall(r"\b\w+\b", text) if token.isalpha()]
         return tokens
 
     @property
-    def models(self) -> list[Word2Vec]:
-        if self._models is None:
-            models = list()
-            for messages in tqdm(self.slices, desc="Training a word2vec model for each slice..."):
+    def name_to_model(self) -> dict[str, Word2Vec]:
+        if self._name_to_model is None:
+            name_to_model = dict()
+            for name, messages in tqdm(self.name_to_slice.items(), desc="Training a word2vec model for each slice..."):
+                logger.info(f"Now preparing a model for {name}")
                 tokenized_sentences = [self.normalize(sentence) for sentence in messages]
-                model = Word2Vec(sentences=tokenized_sentences, vector_size=300, window=5, min_count=10)
-                models.append(model)
+                texts = list()
+                for i in range(len(tokenized_sentences)):
+                    new_text = list()
+                    for sentence in tokenized_sentences[i-20:i+20]:
+                        new_text.extend(sentence)
+                    texts.append(new_text)
+                model = Word2Vec(sentences=tqdm(texts), vector_size=300, window=20, min_count=10)
+                name_to_model[name] = model
 
-            self._models = models
+            self._name_to_model = name_to_model
 
-        return self._models
+        return self._name_to_model
 
-    def get_changes_between_two_models(self, model_i: int, model_j: int, top_n: int) -> list[str]:
-        keyed_vectors_i = self.models[model_i].wv
-        keyed_vectors_j = self.models[model_j].wv
+    def get_changes_between_two_models(self, name1: str, name2: str, top_n: int) -> list[str]:
+        keyed_vectors_1 = self.name_to_model[name1].wv
+        keyed_vectors_2 = self.name_to_model[name2].wv
 
-        keys_i = set(keyed_vectors_i.key_to_index.keys())
-        keys_j = set(keyed_vectors_j.key_to_index.keys())
+        keys_1 = set(keyed_vectors_1.key_to_index.keys())
+        keys_2 = set(keyed_vectors_2.key_to_index.keys())
 
-        common_keys = sorted(keys_i & keys_j)
+        common_keys = sorted(keys_1 & keys_2)
 
-        matrix_i = np.vstack([keyed_vectors_i.get_vector(key) for key in common_keys])
-        matrix_j = np.vstack([keyed_vectors_j.get_vector(key) for key in common_keys])
+        matrix_1 = np.vstack([keyed_vectors_1.get_vector(key) for key in common_keys])
+        matrix_2 = np.vstack([keyed_vectors_2.get_vector(key) for key in common_keys])
 
-        mapping, _ = scipy.linalg.orthogonal_procrustes(matrix_i, matrix_j)
-        matrix_i_aligned = matrix_i @ mapping
+        mapping, _ = scipy.linalg.orthogonal_procrustes(matrix_1, matrix_2)
+        matrix_1_aligned = matrix_1 @ mapping
 
-        scores = (matrix_i_aligned * matrix_j).sum(-1)
+        scores = (matrix_1_aligned * matrix_2).sum(-1)
         indices_sorted_by_score = scores.argsort()
 
         changed_words_indices = indices_sorted_by_score[:top_n]
@@ -141,8 +149,11 @@ class ShiftsDetector:
 
 def main():
     shifts_detector = ShiftsDetector()
-    words = shifts_detector.get_changes_between_two_models(-1, -2, 10)
-    logger.info(f"The most changed words are: {', '.join(words)}")
+    names = list(shifts_detector.name_to_slice.keys())
+    for i in range(len(names) - 1):
+        name1, name2 = names[i], names[i+1]
+        words = shifts_detector.get_changes_between_two_models(name1, name2, 10)
+        logger.info(f"The most changed words from {name1} to {name2} are: {', '.join(words)}")
 
 
 if __name__ == '__main__':
